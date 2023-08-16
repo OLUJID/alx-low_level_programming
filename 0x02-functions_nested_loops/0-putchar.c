6f62ab94d87b921de29a5032c241fccb647c4266
OLUJID <belovejide@gmail.com>
2023-08-16 10:14:47 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,6 @@
 #include <stdio.h>
-#include "main.h"
-
-int _putchar(char c) {
-    return putchar(c);
-}
 
 int main() {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-
+    printf("_putchar\n");
     return 0;
 }
