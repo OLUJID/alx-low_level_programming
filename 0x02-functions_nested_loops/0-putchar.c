f3a15fddc06a086d32752949b0b76f43e321fdfb
OLUJID <belovejide@gmail.com>
2023-08-16 10:06:19 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Custom _putchar function
+int _putchar(char c) {
+    return putchar(c);
+}
+
+int main() {
+    // Calling _putchar to print each character
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n'); // New line
+
+    return 0;
+}
