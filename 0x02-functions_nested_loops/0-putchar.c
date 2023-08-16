c51334bccd1fa95be7f52e3a7e0dd9621888fa9a
OLUJID <belovejide@gmail.com>
2023-08-16 12:05:17 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * main -Entry point
+ * main - Entry point
  * Return: Always 0
  */
-
-int main(void) 
+int main(void)
 {
     _putchar('_');
     _putchar('p');
@@ -17,5 +16,5 @@ int main(void)
     _putchar('r');
     _putchar('\n');
 
-    return 0;
+    return (0);
 }
