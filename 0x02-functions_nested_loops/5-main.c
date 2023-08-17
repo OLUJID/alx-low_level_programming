d99a9a71f5e93f41be27ec8a4180a685a4c1bd9e
OLUJID <belovejide@gmail.com>
2023-08-17 11:07:35 -0700

    6-abs.c

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -1,26 +1,33 @@
-/**
- * print_sign - prints the sign of a number
- * @n: the int to check
- * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
- */
+#include "main.h"
 
-int _print_sign(int n)
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-    if (n > 0)
-    {
-        putchar('+');
-        return 1;
-    }
-    else if (n == 0)
-    {
-        putchar('0');
-        return 0;
-    }
-    else
-    {
-        putchar('-');
-        return -1;
-    }
+    int r;
+
+    r = _print_sign(98);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(0);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(0xff);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(-1);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
 }
