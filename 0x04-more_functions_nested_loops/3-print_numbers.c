32ba4aa6e6cd9aa02ebfa3d3214fc37adf45feb2
OLUJID <belovejide@gmail.com>
2023-08-18 08:12:50 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,17 +2,17 @@
 
 /**
  * print_numbers - Prints the numbers from 0 to 9, followed by a new line.
- * Code - Written by Olujide Fawole/
+ * Code - Written by Olujide Fawole
+ */
 void print_numbers(void)
 {
-    char digit = '0'; /* Initialize with the character '0'*/
+    char digit = '0'; /* Starting with the character '0' */
 
-    while (digit <= '9') /* Loop from '0' to '9' */
+    while (digit <= '9') /* Looping start from '0' to '9' */
     {
-        _putchar(digit); /* Print the current digit */
-        digit++; /* Move to the next character ('1', '2', ...)*/
+        _putchar(digit); /* The current digit is to be printed */
+        digit++; /* Move to the next character ('1', '2', ...) */
     }
 
     _putchar('\n'); /* Print a newline character after printing digits */
-
 }
