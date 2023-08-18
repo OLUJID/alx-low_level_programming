1dfa815bc443b2e9f80ce6bed89765b944b97764
OLUJID <belovejide@gmail.com>
2023-08-18 10:04:47 -0700

    ok

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * The program to multiply two numbers
+ */
+
+void print_square (int m)
+{
+	a, b;
+	if (m <= 0)
+	{
+		_putchar ('\n')
+		{
+		else
+		{
+		for ( a = 0; a < m; a++)
+		{
+		for ( b = 0; b < m; b++)
+		} 
+		_putchar (35);
+		}
+		_putchar ('\n');
+		}
+	}
+}
