8d557bbd1275be1b3e77e1181bb4eed1e51b1ab5
OLUJID <belovejide@gmail.com>
2023-08-18 06:41:43 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,18 @@
-int _isupper(int c) {
-    if (c >= 'A' && c <= 'Z') {
-        return 1; // c is an uppercase character
-    } else {
-        return 0; // c is not an uppercase character
+#include "main.h"
+
+/**
+ * _isupper - Check if a character is uppercase
+ * @c: The character to be checked
+ * Return: 1 if the character is uppercase, 0 otherwise
+ * Written by: Fawole Olujide
+ */
+
+int _isupper(int c)
+{
+    if (c >= 65 && c <= 90)
+    {
+        return 1;
     }
+        return 0;
+    
 }
