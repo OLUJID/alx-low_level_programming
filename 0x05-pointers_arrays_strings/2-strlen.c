5210f62a2296b87a837c6d80eca2fa6e8218f469
OLUJID <belovejide@gmail.com>
2023-08-21 18:05:47 -0700

    a

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,11 +5,10 @@
  * @str: The string to get the length of
  * Return: The length of @str.
  */
-
 size_t _strlen(const char *str)
 {
 	size_t length = 0;
 	while (*str++)
 		length++;
-	return(length);
+	return (length);
 }
