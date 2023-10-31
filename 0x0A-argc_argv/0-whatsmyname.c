425fc70c38c3bba0c6e05e457210cd5d1176155e
OLUJID <belovejide@gmail.com>
2023-10-31 08:50:21 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - print the name of the program
+ *@argc: it represents the argument counts
+ *@argv: it represents the argument vectors
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s/n", argv[0]);
+	return (0);
+}
+
